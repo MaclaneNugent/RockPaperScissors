60b2dfc1a063de7b4595f9011bfb5f3daf6538f1
MaclaneNugent <mnugent@acumensolutions.com>
2019-03-04 13:00:49 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,74 @@
+var options = ['Rock', 'Paper', 'Scissors'];
+var playerScore = 0;
+var computerScore = 0;
+
+function startRound(playerChoice) {
+	var computersChoice = getComputersChoice();
+	var roundWinner = getRoundWinner(playerChoice, computersChoice);
+
+	setPoints(roundWinner)
+	checkForWinner(playerScore, computerScore);
+}
+
+function getComputersChoice() {
+	return options[Math.floor(Math.random() * 3)];
+}
+
+function getRoundWinner(playerChoice, computersChoice) {
+	if ((playerChoice == 'Rock' && computersChoice == 'Scissors') || 
+		(playerChoice == 'Paper' && computersChoice == 'Rock') || 
+		(playerChoice == 'Scissors' && computersChoice == 'Paper')) {
+
+		document.getElementById("round").innerHTML = 'WIN';
+		return 'WIN';
+	} 
+	else if ((computersChoice == 'Rock' && playerChoice == 'Scissors') || 
+		(computersChoice == 'Paper' && playerChoice == 'Rock') || 
+		(computersChoice == 'Scissors' && playerChoice == 'Paper')) {
+
+		document.getElementById("round").innerHTML = 'LOSE';
+		return 'LOSE';
+	}
+	else {
+		document.getElementById("round").innerHTML = 'TIE';
+		return 'TIE';
+	}
+}
+
+function setPoints(roundWinner) {
+	if (roundWinner == 'WIN') {
+		playerScore ++;
+		document.getElementById("player").innerHTML = playerScore;
+	}
+	else if (roundWinner == 'LOSE') {
+		computerScore ++;
+		document.getElementById("computer").innerHTML = computerScore;
+	}
+	else {
+
+	}
+}
+
+function checkForWinner(playerScore, computerScore) {
+	if (playerScore == 5) {
+		document.getElementById("round").innerHTML = 'YOU WIN';
+		disableButtons();
+	}
+	else if (computerScore == 5) {
+		document.getElementById("round").innerHTML = 'YOU LOSE';
+		disableButtons();
+	}
+	else {
+		console.log('keep going');
+	}
+}
+
+function disableButtons() {
+	var elems = document.getElementsByClassName("buttons");
+	for(var i = 0; i < elems.length; i++) {
+	    elems[i].disabled = true;
+	}
+}
+
+
+
